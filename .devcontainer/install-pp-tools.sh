294ef62793d301f12a80c7a017369cb7f4c3af06
Hjalmar Otto Fjøsne <hjaf@equinor.com>
2022-11-21 16:30:07 +0000

    configuration, documentation examples

---
+++ .devcontainer/install-pp-tools.sh
@@ -0,0 +1,6 @@
+pip3 install --user paconn
+wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+git config --global --add safe.directory /workspaces/PowerPlatformFusionDev
+sudo apt-get update && sudo apt install -y --allow-downgrades aspnetcore-runtime-6.0=6.0.8-1 dotnet-apphost-pack-6.0=6.0.8-1 dotnet-host=6.0.8-1 dotnet-hostfxr-6.0=6.0.8-1 dotnet-runtime-6.0=6.0.8-1 dotnet-sdk-6.0=6.0.400-1 dotnet-targeting-pack-6.0=6.0.8-1
